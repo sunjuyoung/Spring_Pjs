7f81d667dcb2e2a9e83fc3cb2e3b45d58eabee73
Sun <syseoz@naver.com>
2020-12-06 23:14:28 +0900

    item

--- jpaWeb/src/main/java/jpabook/jpaWeb/domain/item/Item.java
+++ jpaWeb/src/main/java/jpabook/jpaWeb/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpaWeb.domain.item;
 
 import jpabook.jpaWeb.domain.Category;
+import jpabook.jpaWeb.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,27 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categoryList = new ArrayList<>();
+
+    //비즈니스 로직
+
+    /**
+     * 제고 증가
+     * @param quantity
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 제고 감소
+     * @param quantity
+     */
+    public void minusStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock <0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity= restStock;
+
+    }
 }
