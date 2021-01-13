6fd019318c0ef0483729eae7e3e86bf0cd005bd4
Sun <syseoz@naver.com>
2021-01-13 23:12:09 +0900

    thymeleaf

--- ex3/src/main/java/com/test/controller/SampleController.java
+++ ex3/src/main/java/com/test/controller/SampleController.java
@@ -1,4 +1,66 @@
 package com.test.controller;
 
+import com.test.dto.SampleDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
 public class SampleController {
+
+    @GetMapping("/ex1")
+    public void ex1(){
+
+    }
+
+    @GetMapping({"/ex2","/exLink"})
+    public void ex2(Model model){
+        List<SampleDto> list = IntStream.rangeClosed(1,10).asLongStream().mapToObj(i->{
+                SampleDto dto = SampleDto.builder()
+                        .sno(i)
+                        .first("first : " + i)
+                        .last("last : " +i)
+                        .regTime(LocalDateTime.now())
+                        .build();
+                return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list",list);
+    }
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes){
+        SampleDto dto = SampleDto.builder()
+                .sno(100L)
+                .first("first : " + 100L)
+                .last("last : " +100L)
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result","success");
+        redirectAttributes.addFlashAttribute("dto",dto);
+
+        return "redirect:/sample/ex3";
+
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+        
+    }
+
+    @GetMapping("/exLayout1")
+    public void exLayout1(){
+
+    }
 }
--- ex3/src/main/java/com/test/dto/SampleDto.java
+++ ex3/src/main/java/com/test/dto/SampleDto.java
@@ -1,4 +1,17 @@
 package com.test.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
 public class SampleDto {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+
 }
