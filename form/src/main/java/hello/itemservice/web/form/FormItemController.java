a9da8fef634f0f0d7c335a00f87dbffeaf79e1cf
Sun <syseoz@naver.com>
2021-07-05 18:49:10 +0900

    thymeleaf form

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,7 +1,9 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +32,22 @@ public class FormItemController {
         regions.put("BUSAN","부산");
 
         return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
+
+
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCode(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST","빠른배송"));
+        deliveryCodes.add(new DeliveryCode("NOMAL","보통배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW","느린배송"));
+        return deliveryCodes;
     }
 
     @GetMapping
